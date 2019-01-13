6de63a765801144eb7d1368ef84a25ed1e7bf80f
Patryk Proniewicz <pproniew@gmail.com>
2019-01-13 21:05:36 +0100

    bayes network using, gui

---
+++ src/Bayes.java
@@ -0,0 +1,44 @@
+import smile.Network;
+
+import java.util.Map;
+
+public class Bayes {
+
+    private Network network;
+
+    public Bayes(){
+        enableLicense();
+        network = new Network();
+        network.readFile("projekt.xdsl");
+    }
+
+//    public static void main(String[] args) {
+//        Network network = new Network();
+//        network.readFile("projekt.xdsl");
+//        network.setEvidence("banjo", "tak");
+//        network.setEvidence("harmonijka", "tak");
+//        network.updateBeliefs();
+//        double[] genres = network.getNodeValue("gatunek");
+//        for(int i=0; i < genres.length; i++) {
+//            System.out.println(network.getOutcomeId("gatunek", i) + ": " + genres[i]);
+//        }
+//
+//        //network.clearAllEvidence();
+//    }
+
+    public String getResult(Map<String, String> nodes) {
+        network.clearAllEvidence();
+        nodes.forEach((option, evidence) -> network.setEvidence(option, evidence));
+        network.updateBeliefs();
+        double[] genres = network.getNodeValue("gatunek");
+        StringBuilder result = new StringBuilder();
+        for(int i=0; i < genres.length; i++) {
+            result.append(network.getOutcomeId("gatunek", i) + ": " + genres[i] + "\n");
+        }
+        return result.toString();
+    }
+
+    private void enableLicense() {
+
+    }
+}
---
+++ src/GUI.java
@@ -0,0 +1,103 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.*;
+import java.util.List;
+
+public class GUI extends JPanel
+        implements ItemListener {
+    private final List<String> nodeNames;
+    JCheckBox chinButton;
+    JCheckBox glassesButton;
+    JCheckBox hairButton;
+    JCheckBox teethButton;
+
+    private Map<String, List<JCheckBox>> options = new HashMap<>();
+    private Map<String, String> selectedOptions = new HashMap<>();
+
+    StringBuffer choices;
+
+    public GUI() {
+        super(new GridLayout(8, 2));
+
+        Bayes bayes = new Bayes();
+
+        JPanel checkPanel = new JPanel(new GridLayout(5, 3));
+
+        nodeNames = Arrays.asList("gitara_elektryczna", "gitara_basowa", "gitara_akustyczna", "harmonijka", "banjo", "skrzypce", "harfa", "pianino", "saksofon", "trabka", "kontrabas", "wokal", "taneczny");
+
+        for (String s : nodeNames) {
+            JCheckBox jCheckBoxYes = new JCheckBox(s);
+            JCheckBox jCheckBoxNo = new JCheckBox(s);
+            ArrayList<JCheckBox> checkBoxes = new ArrayList<>();
+            checkBoxes.add(jCheckBoxYes);
+            checkBoxes.add(jCheckBoxNo);
+            options.put(s, checkBoxes);
+            checkPanel.add(jCheckBoxYes);
+            checkPanel.add(jCheckBoxNo);
+        }
+
+        JLabel label = new JLabel();
+
+
+        JButton submit = new JButton("Submit");
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText("");
+                selectedOptions.clear();
+                getCheckBoxResult();
+                label.setText(bayes.getResult(selectedOptions));
+            }
+        });
+
+
+        add(new Label("Opcje"));
+        add(checkPanel);
+        add(new JPanel());
+        add(submit);
+        add(label);
+
+        setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+    }
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("CheckBoxDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        JComponent newContentPane = new GUI();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> createAndShowGUI());
+    }
+
+    public void getCheckBoxResult() {
+        options.forEach((option, checkBoxes) -> putValueToMap(option, checkBoxes));
+
+    }
+
+    private void putValueToMap(String option, List<JCheckBox> checkBoxes) {
+        if (checkBoxes.get(0).isSelected()) {
+            selectedOptions.put(option, "tak");
+        } else if (checkBoxes.get(1).isSelected()) {
+            selectedOptions.put(option, "nie");
+        }
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+
+    }
+}
